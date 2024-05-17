ccf4c39864579731bc7c99b08a519005481db2e5
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2024-05-17 18:24:38 +1000

    Site generation functional
    * build-site.el:

--- build-site.el
+++ build-site.el
@@ -16,25 +16,81 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+;; Define the date format.
+(defvar this-date-format "%b %d, %Y")
+
+;; Imports the preamble html from preamble.html
+(defun preamble (plist)
+  ;; "PLIST: An entry."
+  ;; (if (org-export-get-date plist this-date-format)
+  ;;     (plist-put plist
+  ;;                :subtitle (format "Published on %s by %s."
+  ;;                                  (org-export-get-date plist this-date-format)
+  ;;                                  (car (plist-get plist :author)))))
+  ;; ;; Preamble
+  (with-temp-buffer
+    (insert-file-contents "../html-template/preamble.html") (buffer-string)))
+
+;; Imports the postamble html from postamble.html
+(defun postamble (plist)
+  ;; "PLIST: An entry."
+  ;; (if (org-export-get-date plist this-date-format)
+  ;;     (plist-put plist
+  ;;                :subtitle (format "Published on %s by %s."
+  ;;                                  (org-export-get-date plist this-date-format)
+  ;;                                  (car (plist-get plist :author)))))
+  ;; ;; Postamble
+  (with-temp-buffer
+    (insert-file-contents "../html-template/postamble.html") (buffer-string)))
+
 
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "org-site:main"
-             :recursive t
-             :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc nil                ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+       (list "main"
+	     :recursive t
+	     :base-directory "./main"
+	     :publishing-function 'org-html-publish-to-html
+	     :publishing-directory "./public"
+	     :with-author nil
+	     :with-creator nil
+	     :with-toc nil
+	     :section-numbers nil
+	     :html-validation-link nil
+	     :html-head-include-scripts nil
+	     :html-head-include-default-style nil
+	     :time-stamp-file nil
+	     :html-head "<link rel=\"stylesheet\" href=\"./css/simple.css\" />"
+	     :html-preamble 'preamble
+	     :html-postamble 'postamble)
+       (list "posts"
+	     :recursive t
+	     :base-directory "./posts"
+	     :publishing-function 'org-html-publish-to-html
+	     :publishing-directory "./public/posts"
+	     :with-author nil
+	     :with-creator nil
+	     :with-toc nil
+	     :section-numbers nil
+	     :html-validation-link nil
+	     :html-head-include-scripts nil
+	     :html-head-include-default-style nil
+	     :time-stamp-file nil
+	     :html-head "<link rel=\"stylesheet\" href=\"../css/simple.css\" />"
+	     :html-preamble 'preamble
+	     :html-postamble 'postamble)
+       (list "css"
+	     :recursive t
+	     :base-directory "./css"
+	     :base-extension "css"
+	     :publishing-function 'org-publish-attachment
+	     :publishing-directory "./public/css/")
+       (list "attachment"
+	     :recursive t
+	     :base-directory "./attachment"
+	     :base-extension '("webm")
+	     :publishing-function 'org-publish-attachment
+	     :publishing-directory "./public/attachment/")))
 
 ;; Generate the site output
 (org-publish-all t)
