8780ff7839e0865ef2a2fe4c27d531cb3528a091
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2026-09-07 09:58:36 +0000

    Added RSS and first post.
    * blog/generating.org:

--- build-site.el
+++ build-site.el
@@ -16,30 +16,120 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-;; Define the date format.
-(defvar this-date-format "%b %d, %Y")
+(use-package org-publish-rss
+  :vc (:url "https://git.sr.ht/~taingram/org-publish-rss"
+       :rev :newest))
+
+;; Macro and function sourced from https://taingram.org/blog/org-mode-blog.html
+(setq org-export-global-macros
+            '(("timestamp" . "@@html:<span class=\"timestamp\">[$1]</span>@@")))
+
+(defun my/org-sitemap-date-entry-format (entry style project)
+  "Format ENTRY in org-publish PROJECT Sitemap format ENTRY ENTRY STYLE format that includes date."
+  (let ((filename (org-publish-find-title entry project)))
+    (if (= (length filename) 0)
+        (format "*%s*" entry)
+      (format "{{{timestamp(%s)}}} [[file:%s][%s]]"
+              (format-time-string "%Y-%m-%d"
+                                  (org-publish-find-date entry project))
+              entry
+              filename))))
 
 ;; Imports the preamble html from preamble.html
-(defun preamble (plist)
+(defvar preamble
   (with-temp-buffer
-    (insert-file-contents "../html-template/preamble.html") (buffer-string)))
+    (insert-file-contents "./files/html-template/preamble.html")
+    (buffer-string))
+  "Contents of preamble.html.")
+
+(setq org-html-postamble preamble)
 
 ;; Imports the postamble html from postamble.html
-(defun postamble (plist)
+(defvar postamble
   (with-temp-buffer
-    (insert-file-contents "../html-template/postamble.html") (buffer-string)))
+    (insert-file-contents "./files/html-template/postamble.html")
+    (buffer-string))
+  "Contents of postamble.html.")
+
+(setq org-html-postamble postamble)
+
+;; Import the head from head.html
+(defvar head
+  (with-temp-buffer
+    (insert-file-contents "./files/html-template/head.html")
+    (buffer-string))
+  "Contents of head.html.")
+
+(setq org-html-head head)
+
+(setq org-html-home/up-format "")
 
 (setq org-html-divs '((preamble "header" "top")
                       (content "main" "content")
-                      (postamble "footer" "postamble"))
-      org-html-metadata-timestamp-format this-date-format)
+                      (postamble "footer" "postamble")))
 
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
+       (list "blog"
+	     :base-directory "./files/blog"
+	     :publishing-function 'org-html-publish-to-html
+	     :publishing-directory "./public/blog"
+	     :exclude "index.org"
+	     :with-title nil
+	     :with-author t
+	     :with-creator t
+	     :with-date t
+	     :with-toc nil
+	     :section-numbers nil
+	     :html-validation-link nil
+	     :html-doctype "html5"
+	     :html-html5-fancy t
+	     :html-head-include-scripts nil
+	     :html-head-include-default-style nil
+	     :time-stamp-file nil
+	     :html-head head
+	     :html-preamble preamble
+	     :html-postamble postamble
+	     
+	     :auto-sitemap t
+	     :sitemap-format-entry 'my/org-sitemap-date-entry-format
+	     :sitemap-title "Blog Posts"
+	     :sitemap-filename "sitemap.org"
+	     :sitemap-sort-files 'anti-chronologically
+	     
+	     :auto-rss t
+	     :rss-file "blog.xml"
+	     :rss-title "Jacob Moujalli's Blog Posts"
+	     :rss-description "Blog posts on various topics."
+	     :html-link-home "https://moujal.li"
+	     :rss-with-content 'all
+	     :completion-function 'org-publish-rss)
+
+       (list "blog-index"
+	     :base-directory "./files/blog"
+	     :publishing-function 'org-html-publish-to-html
+	     :publishing-directory "./public/blog"
+	     :include '("index.org")
+	     :exclude ".*"
+	     :with-title nil
+	     :with-author t
+	     :with-creator t
+	     :with-date t
+	     :with-toc nil
+	     :section-numbers nil
+	     :html-validation-link nil
+	     :html-doctype "html5"
+	     :html-html5-fancy t
+	     :html-head-include-scripts nil
+	     :html-head-include-default-style nil
+	     :time-stamp-file nil
+	     :html-head head
+	     :html-preamble preamble
+	     :html-postamble postamble)
+
        (list "main"
-	     :recursive t
-	     :base-directory "./main"
+	     :base-directory "./files"
 	     :publishing-function 'org-html-publish-to-html
 	     :publishing-directory "./public"
 	     :with-title nil
@@ -48,40 +138,33 @@
 	     :with-toc nil
 	     :section-numbers nil
 	     :html-validation-link nil
+	     :html-doctype "html5"
+	     :html-html5-fancy t
 	     :html-head-include-scripts nil
 	     :html-head-include-default-style nil
 	     :time-stamp-file nil
-	     :html-head "<link rel=\"stylesheet\" href=\"./css/simple.css\" />"
-	     :html-preamble 'preamble
-	     :html-postamble 'postamble)
-       (list "blog"
+	     :html-head head
+	     :html-link-home "https://moujal.li"
+	     :html-preamble preamble
+	     :html-postamble postamble)
+
+       (list "rss"
 	     :recursive t
-	     :base-directory "./blog"
-	     :publishing-function 'org-html-publish-to-html
-	     :publishing-directory "./public/blog"
-	     :with-title nil
-	     :with-author t
-	     :with-creator t
-	     :with-date t
-	     :with-toc nil
-	     :section-numbers nil
-	     :html-validation-link nil
-	     :html-head-include-scripts nil
-	     :html-head-include-default-style nil
-	     :time-stamp-file nil
-	     :html-head "<link rel=\"stylesheet\" href=\"../css/simple.css\" />"
-	     ;; :html-head "<link rel='icon' type='image/x-icon' href='../attachment/favicon.ico'/>"
-	     :html-preamble 'preamble
-	     :html-postamble 'postamble)
+	     :base-directory "./files"
+	     :base-extension "xml"
+	     :publishing-function 'org-publish-attachment
+	     :publishing-directory "./public/")
+
        (list "css"
 	     :recursive t
-	     :base-directory "./css"
+	     :base-directory "./files"
 	     :base-extension "css"
 	     :publishing-function 'org-publish-attachment
-	     :publishing-directory "./public/css/")
+	     :publishing-directory "./public/")
+
        (list "assets"
 	     :recursive t
-	     :base-directory "./assets"
+	     :base-directory "./files/assets/"
 	     :base-extension 'any'
 	     :publishing-function 'org-publish-attachment
 	     :publishing-directory "./public/assets/")))
